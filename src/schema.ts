650b6ffa782f909f34bc62935017a1d9c82bfabd
shakoori-naeimeh <shakoori.naeimeh@gmail.com>
2024-07-12 15:33:05 -0400

    Initial setup and creation of the GraphQL server

---
+++ src/schema.ts
@@ -0,0 +1,14 @@
+import 'graphql-import-node';
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import typeDefs from "./schema.graphql";
+
+const resolvers = {
+  Query: {
+    getVehicle: () => 'Test',
+  }
+}
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
